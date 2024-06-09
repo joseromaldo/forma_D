b65a87a8f5fd14e4c52732c922142abd0d4af4b0
Maldonado <joseromaldo.1997@gmail.com>
2024-06-09 03:55:55 -0600

    Modificación de la base de datos

--- script SQL/script.sql
+++ script SQL/script.sql
@@ -1,38 +1,62 @@
--- SCRIPT EXAMEN FINAL INGENERÍA DE SOFTWARE
-CREATE DATABASE company WITH LOG;
--- TABLA PARA EL REGISTRO DE PROGRAMADORES
+
+CREATE  database company;
+
+
+CREATE TABLE  grados (
+    gra_id SERIAL NOT NULL,
+    gra_nombre VARCHAR(50) NOT NULL,
+    gra_sit SMALLINT DEFAULT 1,
+    PRIMARY KEY (gra_id)
+);
+
+
+CREATE TABLE armas (
+arma_id SERIAL NOT NULL,
+arma_descripcion VARCHAR(50) NOT NULL,
+arma_situacion SMALLINT DEFAULT 1,
+PRIMARY KEY (arma_id)
+
+);
+
+
 CREATE TABLE programador(
 pro_id SERIAL NOT NULL,
-pro_grado VARCHAR (50),
-pro_arma VARCHAR (50),
-pro_nombre VARCHAR (50),
-pro_apellido VARCHAR (50),
-pro_correo VARCHAR (50),
-PRIMARY KEY (pro_id)
+pro_grado INTEGER NOT NULL,------------
+pro_arma INTEGER NOT NULL,
+pro_nombre VARCHAR (50),---------
+pro_apellido VARCHAR (50),-------------
+pro_correo VARCHAR (50),---------
+pro_dpi  VARCHAR (50) NOT NULL,
+pro_situacion SMALLINT DEFAULT 1,
+PRIMARY KEY (pro_id),
+FOREIGN KEY (pro_grado) REFERENCES grados (gra_id),
+FOREIGN key (pro_arma) REFERENCES armas (arma_id),
+UNIQUE  (pro_dpi)
+
 );
--- TABLA PARA EL REGISTRO DE APLICACIONES
+
 CREATE TABLE aplicacion(
 apl_id SERIAL NOT NULL,
 apl_nombre VARCHAR (50),
 apl_estado SMALLINT DEFAULT 1,
-apl_descripcion VARCHAR (100),
-apl_fechaentrega DATETIME YEAR TO DAY,
 PRIMARY KEY (apl_id)
 );
--- TABLA PARA EL CONTROL DE ASIGNACIONES
+
 CREATE TABLE asignacion(
 asi_id SERIAL NOT NULL,
 asi_programador INTEGER,
 asi_aplicacion INTEGER,
+asi_situacion SMALLINT DEFAULT 1,
 PRIMARY KEY (asi_id),
 FOREIGN KEY (asi_programador) REFERENCES programador (pro_id),
 FOREIGN KEY (asi_aplicacion) REFERENCES aplicacion (apl_id)
 );
--- TABLA PARA EL CONTROL DE TAREAS Y PROGRESO DEL TRABAJO
-CREATE TABLE tarea(
-tar_id SERIAL NOT NULL,
-tar_nombre VARCHAR (50),
-tar_fecha DATETIME YEAR TO DAY,
-tar_asignacion INTEGER,
-tar_estado DECIMAL (10,2) 
+
+Create table tareas (
+    tar_id serial not null primary key,
+    tar_app int not null,
+    tar_descripcion varchar (50) not null,
+    tar_fecha date not null,
+    tar_estado char (1) DEFAULT '1',
+    foreign key (tar_app) REFERENCES aplicaciones(app_id)
 );
