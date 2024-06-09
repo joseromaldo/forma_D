06137479b55fc13ae683bd98f32c057c72c10eb9
Maldonado <joseromaldo.1997@gmail.com>
2024-06-09 04:14:20 -0600

    Creación de footer, header y navbar

---
+++ vistas/templates/navbar.php
@@ -0,0 +1,109 @@
+<nav class="navbar fixed-top navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="/forma_D/src/images/logo.jpeg" alt="Logo" width="30" height="30">Control Alumnos</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house-fill me-2"></i>Inicio</a>
+        </li>
+   
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Grado
+          </a>
+          <ul class="dropdown-menu bg-dark">
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/grados/index.php">Crear Grado</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/grados/buscar.php">Buscar Grado</a>
+            </li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Arma
+          </a>
+          <ul class="dropdown-menu bg-dark">
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/armas/index.php">Crear Arma</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/armas/buscar.php">Buscar Arma</a>
+            </li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Programadores
+          </a>
+          <ul class="dropdown-menu bg-dark">
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/programador/index.php">Registrar programadores</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/programador/buscar.php">Buscar programadores</a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Aplicaciones
+          </a>
+          <ul class="dropdown-menu bg-dark">
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/aplicaciones/index.php">Crear aplicaciones</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/aplicaciones/buscar.php">Buscar aplicaciones</a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Tareas
+          </a>
+          <ul class="dropdown-menu bg-dark">
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/tareas/index.php">Crear Tareas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/tareas/buscar.php">Buscar Tareas</a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Asignar
+          </a>
+          <ul class="dropdown-menu bg-dark">
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/asignar/index.php"> Asignar aplicaciones</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/asignar/buscar.php">Buscar asignaciones</a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link  dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Detalle
+          </a>
+          <ul class="dropdown-menu bg-dark">
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/detalle/index.php">Registro de detalles </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/controlAlumnos/vistas/detalle/buscar.php">Buscar detalles</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
