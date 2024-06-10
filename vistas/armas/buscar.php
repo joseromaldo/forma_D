0aa2369c19e291f7a2b65fc8a8a2ecaea12b1b30
Maldonado <joseromaldo.1997@gmail.com>
2024-06-09 22:34:13 -0600

    Modificación de vistas para las armas

--- vistas/armas/buscar.php
+++ vistas/armas/buscar.php
@@ -1,22 +1,23 @@
 <?php include_once '../templates/header.php'?>
 <?php include_once '../templates/navbar.php'?>
-    <div class="container">
-        <h1 class="text-center">Buscar Armas</h1>
-        <div class="row justify-content-center">
-            <form action="/forma_D/controladores/armas/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
-                <div class="row mb-3">
-                    <div class="col">
-                        <label for="arma_descripcion">Armas</label>
-                        <input type="text" name="arma_descripcion" id="arma_descripcion" class="form-control" >
-                    </div>
-                </div>
 
-                <div class="row mb-3">
-                    <div class="col">
-                        <button type="submit" class="btn btn-info w-100">Buscar</button>
-                    </div>
-                </div>
-            </form>
-        </div>
+<div class="container mt-5">
+  <div class="card">
+    <div class="card-header">
+      <h1 class="text-center">Buscar Armas</h1>
     </div>
-    <?php include_once '../templates/footer.php'?>
+    <div class="card-body">
+      <form action="/forma_D/controladores/armas/buscar.php" method="GET">
+        <div class="mb-3">
+          <label for="arma_descripcion" class="form-label">Armas</label>
+          <input type="text" name="arma_descripcion" id="arma_descripcion" class="form-control">
+        </div>
+        <div class="text-center">
+          <button type="submit" class="btn btn-info btn-lg w-100"><i class="bi bi-search me-3"></i>Buscar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<?php include_once '../templates/footer.php'?>
--- vistas/armas/index.php
+++ vistas/armas/index.php
@@ -1,21 +1,23 @@
 <?php include_once '../templates/header.php'?>
 <?php include_once '../templates/navbar.php'?>
+
 <div class="container mt-5">
-  <h1 class="text-center mt-3">Arma del Oficial</h1>
-  <div class="row justify-content-center mt-2">
-    <form action="/forma_D/controladores/armas/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
-      <div class="row mb-3">
-        <div class="col">
+  <div class="card">
+    <div class="card-header text-black">
+      <h1 class="text-center">Arma del Oficial</h1>
+    </div>
+    <div class="card-body">
+      <form action="/forma_D/controladores/armas/guardar.php" method="POST">
+        <div class="mb-3">
           <label for="arma_descripcion" class="form-label">Ingrese el arma</label>
           <input type="text" name="arma_descripcion" id="arma_descripcion" class="form-control" required>
         </div>
-      </div>
-      <div class="row mb-3">
-        <div class="col">
-          <button type="submit" class="btn btn-primary w-100">Guardar</button>
+        <div class="text-center">
+          <button type="submit" class="btn btn-primary"><i class="bi bi-floppy me-2"></i>Guardar</button>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
 </div>
+
 <?php include_once '../templates/footer.php'?>
