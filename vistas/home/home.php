4169edf59f75f5ad61dd5b6b0c27c1072f69117b
Maldonado <joseromaldo.1997@gmail.com>
2024-06-09 21:49:34 -0600

    Modificación de Navbar, creación de la página principal Home y se agrega nueva imagen para dicha página

---
+++ vistas/home/home.php
@@ -0,0 +1,20 @@
+<?php include_once '../templates/header.php'?>
+<?php include_once '../templates/navbar.php'?>
+
+<div class="container my-5">
+    <div class="row justify-content-center">
+      <div class="col-md-8 p-0 rounded-3 shadow-lg overflow-hidden" style="background-image: url('../../src/images/foto.jpg'); background-size: cover; background-position: center;">
+        <div class="p-5 text-white text-center" style="background: rgba(0, 0, 0, 0.7);">
+          <h1 class="mb-4">¡Bienvenido a nuestra Plataforma de Asignación de Tareas!</h1>
+          <p class="mb-3">
+            Nos complace darte la bienvenida a nuestra innovadora plataforma, diseñada específicamente para optimizar la asignación de tareas y proyectos a nuestros talentosos programadores. Aquí, encontrarás un entorno intuitivo y eficiente que facilita la gestión y seguimiento de tus proyectos de desarrollo de software.
+          </p>
+          <p>
+            Nuestra misión es asegurarnos de que cada tarea sea asignada de manera precisa y oportuna, permitiendo a nuestros programadores enfocarse en lo que mejor saben hacer: crear soluciones tecnológicas de alta calidad.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php include_once '../templates/footer.php'?>
--- vistas/templates/navbar.php
+++ vistas/templates/navbar.php
@@ -1,106 +1,78 @@
-<nav class="navbar fixed-top navbar-expand-lg bg-light">
+<nav class="navbar fixed-top navbar-expand-lg bg-primary shadow-sm p-3 mb-5 rounded">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="/forma_D/src/images/logo.jpeg" alt="Logo" width="30" height="30">Control Alumnos</a>
+    <a class="navbar-brand me-2 d-flex align-items-center text-white" href="../home/home.php">
+      <img src="/forma_D/src/images/logo.jpeg" alt="Logo" width="30" height="30" class="me-2 rounded"> 
+      <span>Application Control</span>
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house-fill me-2"></i>Inicio</a>
+          <a class="nav-link active text-white" aria-current="page" href="../home/home.php"><i class="bi bi-house-fill me-2"></i>Inicio</a>
         </li>
-   
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Grado
           </a>
           <ul class="dropdown-menu">
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/grados/index.php">Crear Grado</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/grados/buscar.php">Buscar Grado</a>
-            </li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/grados/index.php">Crear Grado</a></li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/grados/buscar.php">Buscar Grado</a></li>
           </ul>
         </li>
-
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Arma
           </a>
           <ul class="dropdown-menu">
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/armas/index.php">Crear Arma</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/armas/buscar.php">Buscar Arma</a>
-            </li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/armas/index.php">Crear Arma</a></li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/armas/buscar.php">Buscar Arma</a></li>
           </ul>
         </li>
-
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Programadores
           </a>
           <ul class="dropdown-menu">
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/programador/index.php">Registrar programadores</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/programador/buscar.php">Buscar programadores</a>
-            </li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/programador/index.php">Registrar programadores</a></li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/programador/buscar.php">Buscar programadores</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Aplicaciones
           </a>
           <ul class="dropdown-menu">
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/aplicaciones/index.php">Crear aplicaciones</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/aplicaciones/buscar.php">Buscar aplicaciones</a>
-            </li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/aplicaciones/index.php">Crear aplicaciones</a></li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/aplicaciones/buscar.php">Buscar aplicaciones</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Tareas
           </a>
           <ul class="dropdown-menu">
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/tareas/index.php">Crear Tareas</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/tareas/buscar.php">Buscar Tareas</a>
-            </li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/tareas/index.php">Crear Tareas</a></li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/tareas/buscar.php">Buscar Tareas</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Asignar
           </a>
           <ul class="dropdown-menu">
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/asignar/index.php"> Asignar aplicaciones</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/asignar/buscar.php">Buscar asignaciones</a>
-            </li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/asignar/index.php">Asignar aplicaciones</a></li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/asignar/buscar.php">Buscar asignaciones</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link  dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Detalle
           </a>
           <ul class="dropdown-menu">
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/detalle/index.php">Registro de detalles </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/forma_D/vistas/detalle/buscar.php">Buscar detalles</a>
-            </li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/detalle/index.php">Registro de detalles</a></li>
+            <li><a class="dropdown-item" href="/forma_D/vistas/detalle/buscar.php">Buscar detalles</a></li>
           </ul>
         </li>
       </ul>
